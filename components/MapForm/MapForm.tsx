a089e54c4b9c2b6c3b7eeafc7d07e796237119cd
Joel Jonsson <Joel.jonsson@columbusglobal.com>
2022-06-03 19:52:39 +0200

    feat: new MapForm component

---
+++ components/MapForm/MapForm.tsx
@@ -0,0 +1,41 @@
+import { FunctionComponent } from 'react'
+
+interface FormElement extends HTMLFormElement {
+  mapId: HTMLInputElement
+}
+
+const MapForm: FunctionComponent = () => {
+  const goToMap = (event: React.FormEvent<FormElement>) => {
+    event.preventDefault()
+    window.location.href = `/map/${event.currentTarget.mapId.value}`
+  }
+  return (
+    <form onSubmit={goToMap}>
+      <label className="mb-2 block" htmlFor="mapId">
+        Type a map id from{' '}
+        <a
+          className="font-bold text-blue-500 hover:underline"
+          href="https://www.cartes.io"
+        >
+          Cartes.io
+        </a>
+      </label>
+      <div className="flex flex-nowrap justify-center gap-2">
+        <input
+          type="string"
+          name="mapId"
+          id="mapId"
+          className="overflow-hidden border-2 border-gray-500 p-2 transition-all hover:bg-blue-50 focus:border-black focus:bg-blue-100"
+        />
+        <button
+          className="border-2 border-gray-500 bg-white px-4 py-2 transition-all hover:border-black hover:bg-blue-50 focus:border-black focus:bg-blue-100"
+          type="submit"
+        >
+          Ok
+        </button>
+      </div>
+    </form>
+  )
+}
+
+export default MapForm
