141162552fc281faa1231de4dbed4adf18d7aede
Joel Jonsson <Joel.jonsson@columbusglobal.com>
2022-06-03 19:58:44 +0200

    feat: add internal Api 'api/markers/[id]'

---
+++ pages/api/markers/[id].ts
@@ -0,0 +1,21 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { id } = req.query
+
+  try {
+    const data = await fetch(`https://cartes.io/api/maps/${id}/markers`)
+    const status: number = data.status
+    if (status === 200) {
+      const json = await data.json()
+      res.status(data.status).json(json)
+    } else if (status === 404) {
+      res.status(404).json({ error: 'markers not found' })
+    }
+  } catch (err) {
+    res.status(500).json({ error: 'failed to load data' })
+  }
+}
